4ff9b8c6a1616abb6f50e462e606968124ee2663
Sergio <sergio.pm.developer@gmail.com>
2023-12-12 20:33:07 +0000

    feat: implement desktop version of the welcome component

---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,73 @@
+import { ButtonWithIcon } from '../../ui/Button'
+import { Github } from '../../icons/GitHub'
+import { Resume } from '../../icons/Resume'
+import styled from 'styled-components'
+import { theme, AppSection } from '../../styles'
+
+// Styled components
+
+const WelcomeContainer = styled.main`
+  max-width: 70rem;
+  height: calc(100vh - 6rem);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+`
+
+const WelcomeTitle = styled.h1`
+  font-family: ${theme.roboto};
+  font-size: ${theme.fontSize10};
+  font-weight: 900;
+  text-align: center;
+  color: ${theme.white};
+  margin-bottom: 2rem;
+  line-height: 1;
+`
+
+const WelcomeDescription = styled.p`
+  font-family: ${theme.roboto};
+  font-size: ${theme.fontSize4};
+  text-align: center;
+  color: ${theme.slate};
+  margin-bottom: 3rem;
+`
+
+const WelcomeButtonsContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+/**
+ * Renders the Welcome component.
+ *
+ * @returns The rendered Welcome component.
+ */
+export function Welcome() {
+  return (
+    <AppSection>
+      <WelcomeContainer>
+        <WelcomeTitle>
+          I'm Sergio Peña and I'm a full stack developer
+        </WelcomeTitle>
+        <WelcomeDescription>
+          Hi! I'm Sergio Peña and I'm a full stack developer. Welcome to my
+          portfolio where you will find everything you need to know about me
+          professionally. Software development is my profession and my passion.
+          Backend, frontend, machine learning, scripting, automation, open
+          source... Everything that has to do with coding is welcome. Let's work
+          together?
+        </WelcomeDescription>
+        <WelcomeButtonsContainer>
+          <a href="https://github.com/sergiopmdeveloper" target="_blank">
+            <ButtonWithIcon text="GitHub" Icon={Github} />
+          </a>
+          <a>
+            <ButtonWithIcon text="Resume" Icon={Resume} />
+          </a>
+        </WelcomeButtonsContainer>
+      </WelcomeContainer>
+    </AppSection>
+  )
+}
---
+++ src/icons/Resume.tsx
@@ -0,0 +1,20 @@
+import { IconProps } from './types'
+
+/**
+ * Renders the Resume icon.
+ * @param {IconProps} props - The icon props.
+ * @param {string} props.className - The class name for the icon.
+ * @returns The rendered Resume icon.
+ */
+export function Resume({ className }: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      viewBox="0 0 16 16"
+    >
+      <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+      <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+    </svg>
+  )
+}
